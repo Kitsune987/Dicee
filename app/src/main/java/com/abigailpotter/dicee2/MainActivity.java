1b9893c5936fedc4a43f5379b3e88b1a9353f171
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2018-12-15 01:49:56 -0500

    Initial Commit

---
+++ app/src/main/java/com/abigailpotter/dicee2/MainActivity.java
@@ -0,0 +1,57 @@
+package com.abigailpotter.dicee2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        final ImageView leftDie = (ImageView) findViewById(R.id.image_leftDie);
+        final ImageView rightDie = (ImageView) findViewById(R.id.image_rightDie);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Dicee2", "Ze Button has been pressed!");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee2", "The random number for the left die is " + number);
+
+                final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+                leftDie.setImageResource(diceArray[number]);
+
+                number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee2", "The random number for the right die is " + number);
+
+                rightDie.setImageResource(diceArray[number]);
+
+            }
+        });
+
+    }
+}
